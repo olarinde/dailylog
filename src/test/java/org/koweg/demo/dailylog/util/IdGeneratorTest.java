71a58e60b986aeb2f13acb9a7c98e4d135132127
olarinde.ajai@gmail.com <olarinde.ajai@gmail.com>
2017-04-17 14:41:49 +0100

    added integrgation test profile

---
+++ src/test/java/org/koweg/demo/dailylog/util/IdGeneratorTest.java
@@ -0,0 +1,14 @@
+package org.koweg.demo.dailylog.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class IdGeneratorTest {
+
+    @Test
+    public void test() {
+        System.out.println("############ TODO ID Generator Test ##############");
+    }
+
+}
---
+++ src/test/java/org/koweg/demo/resources/impl/DailyLogsResourceIntegrationTest.java
@@ -0,0 +1,15 @@
+package org.koweg.demo.resources.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DailyLogsResourceIntegrationTest {
+
+    @Test
+    public void test() {
+        System.out.println("************ TODO Integration Test **************");
+        fail("incomplete test scenario");
+    }
+
+}
